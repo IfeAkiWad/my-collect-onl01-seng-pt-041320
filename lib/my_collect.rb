e80f012735f7696934619dc9805bd99aa131d42a
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-23 04:50:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,10 @@
 
 def my_collect(collection)
-collection = []
-counter = 
-  while condition
-
+new_array = []
+counter = 0
+  while counter < collection.length
+  new_array << yield(collection[counter]) 
+  counter += 1  
   end
+  new_array
+end
